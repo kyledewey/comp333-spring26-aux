7f181642042608be0f7ef76c75fc02b234dc4c1f
Kyle Dewey <kyle.dewey@csun.edu>
2026-01-28 12:05:26 -0800

    .

--- section_1/java/Main.java
+++ section_1/java/Main.java
@@ -3,7 +3,25 @@
 // depending on a command-line argument.
 public class Main {
     // Performs some kind of computation.
-    public static int doComputation() { ... }
+    public static int doComputation() { return 42; }
 
+    // Gives back name of the file to write to, or
+    // null if we aren't writing to a file
+    public static String getFileName(String[] args) {
+        if (args.length >= 1) {
+            return args[0];
+        } else {
+            return null;
+        }
+    }
+    
     public static void main(String[] args) {
-        
+        String fileName = getFileName(args);
+        int result = doComputation();
+        if (fileName == null) {
+            // write to terminal
+            System.out.println(fileName);
+        } else {
+            // write to a file
+            // fileName is specifically the name of the file
+            // to write to
