7807d4a838532dc00759f3b32770cf4159e2730e
Kyle Dewey <kyle.dewey@csun.edu>
2026-01-28 16:49:39 -0800

    .

--- section_2/java/Main.java
+++ section_2/java/Main.java
@@ -1,6 +1,35 @@
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+
 // Does some sort of computation
 // Output will be printed either to the terminal,
 // or to a provided file (as a command-line argument).
 public class Main {
-    public static int doComputation() { ... }
+    public static int doComputation() { return 42; }
+
+    // Gives back name of file to write to, or null
+    // if we are to write to the terminal
+    public static String getFileName(String[] args) {
+        if (args.length > 0) {
+            return args[0];
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName = getFileName(args);
+        int result = doComputation();
+        if (fileName == null) {
+            // write to terminal
+            System.out.println(result);
+        } else {
+            // write to file
+            FileWriter writer = new FileWriter(new File(fileName));
+            writer.write("" + result);
+            writer.close();
+        }
+    }
+}
     
