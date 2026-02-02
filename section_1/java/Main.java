caf95fae8dc4d2c5e8c036aaf3abb488b24787b9
Kyle Dewey <kyle.dewey@csun.edu>
2026-02-02 12:04:15 -0800

    .

--- section_1/java/Main.java
+++ section_1/java/Main.java
@@ -6,38 +6,6 @@ import java.io.IOException;
 // Output is written to a file or to the terminal,
 // depending on a command-line argument.
 public class Main {
-    public static FileWriter writer = null;
-    
-    public static void write(String fileName,
-                             String thingToWrite) throws IOException {
-        if (fileName == null) {
-            // write to terminal
-            System.out.println(thingToWrite);
-        } else {
-            // write to a file
-            // fileName is specifically the name of the file
-            // to write to
-            if (writer == null) {
-                writer = new FileWriter(new File(fileName));
-            }
-            writer.write(thingToWrite);
-        }
-    }
-
-    // Issue: Need to pass fileName through everything
-    // Issue: Creating Writer every time
-    
-    // Performs some kind of computation.
-    public static int doComputation(String fileName) throws IOException {
-        int retval = 0;
-        for (; retval < 10000; retval++) {
-            if (retval % 100 == 0) {
-                write(fileName, "STILL RUNNING: " + retval);
-            }
-        }
-        return retval;
-    }
-
     // Gives back name of the file to write to, or
     // null if we aren't writing to a file
     public static String getFileName(String[] args) {
@@ -48,13 +16,23 @@ public class Main {
         }
     }
     
+    // Performs some kind of computation.
+    public static int doComputation(Writer writer) throws IOException {
+        int retval = 0;
+        for (; retval < 10000; retval++) {
+            if (retval % 100 == 0) {
+                writer.write("STILL RUNNING: " + retval);
+            }
+        }
+        return retval;
+    }
+
     public static void main(String[] args) throws IOException {
         String fileName = getFileName(args);
-        int result = doComputation(fileName);
-        write(fileName, "" + result);
-        if (writer != null) {
-            writer.close();
-        }
+        Writer myWriter = new Writer(fileName);
+        int result = doComputation(myWriter);
+        writer.write("" + result);
+        writer.close();
     }
 }
 
