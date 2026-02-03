7a70c5348eeb9d888a50207bbd2818b1a1cbb53b
Kyle Dewey <kyle.dewey@csun.edu>
2026-02-02 16:28:53 -0800

    .

--- section_2/java/Main.java
+++ section_2/java/Main.java
@@ -1,34 +1,9 @@
-import java.io.FileWriter;
-import java.io.File;
 import java.io.IOException;
 
 // Does some sort of computation
 // Output will be printed either to the terminal,
 // or to a provided file (as a command-line argument).
 public class Main {
-    public static void write(String fileName, String toWrite) throws IOException {
-        if (fileName == null) {
-            // write to terminal
-            System.out.println(toWrite);
-        } else {
-            // write to file
-            // TODO: need to not clobber file
-            FileWriter writer = new FileWriter(new File(fileName));
-            writer.write(toWrite);
-            writer.close();
-        }
-    }
-        
-    public static int doComputation(String fileName) throws IOException {
-        int retval = 0;
-        for (; retval < 10000; retval++) {
-            if (retval % 100 == 0) {
-                write(fileName, toWrite);
-            }
-        }
-        return retval;
-    }
-
     // Gives back name of file to write to, or null
     // if we are to write to the terminal
     public static String getFileName(String[] args) {
@@ -39,10 +14,22 @@ public class Main {
         }
     }
 
+    public static int doComputation(Writer writer) throws IOException {
+        int retval = 0;
+        for (; retval < 10000; retval++) {
+            if (retval % 100 == 0) {
+                writer.write("STILL RUNNING");
+            }
+        }
+        return retval;
+    }
+
     public static void main(String[] args) throws IOException {
         String fileName = getFileName(args);
-        int result = doComputation(fileName);
-        write(fileName, "" + result);
+        Writer writer = new Writer(fileName);
+        int result = doComputation(writer);
+        writer.write("" + result);
+        writer.close();
     }
 }
     
