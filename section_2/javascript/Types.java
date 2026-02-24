6547d46dd8ebc84311b9d7f9c2c2536fdcb45f1d
Kyle Dewey <kyle.dewey@csun.edu>
2026-02-23 17:19:42 -0800

    .

---
+++ section_2/javascript/Types.java
@@ -0,0 +1,11 @@
+public class Types {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static String add(String x, String y) {
+        return x + y;
+    }
+    public static double add(double x, double y) {
+        return x + y;
+    }
+}
---
+++ section_2/javascript/types.js
@@ -0,0 +1,5 @@
+// Wednesday: show any return type in Java,
+// weak/strong typing, static/dynamic typing
+function add(x, y) {
+    return x + y;
+}
