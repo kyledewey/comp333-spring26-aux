46dabefe8aed888531c1dea3f34fa8aece9541ec
Kyle Dewey <kyle.dewey@csun.edu>
2026-02-25 12:48:35 -0800

    .

--- section_1/javascript/add.js
+++ section_1/javascript/add.js
@@ -1,3 +1,6 @@
+// FOR NEXT TIME: continue static vs. dynamic typing,
+//                show javap -c result with overloading and
+//                how it differs, strong vs. weak typing
 function add(x, y) {
     return x + y;
 }
