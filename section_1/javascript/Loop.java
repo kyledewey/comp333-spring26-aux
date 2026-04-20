0802a3907943f2293449427f1adc847791002b73
Kyle Dewey <kyle.dewey@csun.edu>
2026-04-20 15:55:08 -0700

    .

---
+++ section_1/javascript/Loop.java
@@ -0,0 +1,12 @@
+public class Loop {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        for (int elem : arr) {
+            System.out.println(elem);
+        }
+        // for (int index = 0; index < arr.length; index++) {
+        //     int elem = arr[index];
+        //     System.out.println(elem);
+        // }
+    }
+}
---
+++ section_1/javascript/loop.js
@@ -0,0 +1,52 @@
+function forEach(arr, f) {
+    for (let index = 0; index < arr.length; index++) {
+        let elem = arr[index];
+        f(elem);
+    }
+}
+function printAll(arr) {
+    forEach(arr, (elem) => console.log(elem));
+}
+function printSome(arr, shouldPrint) {
+    forEach(arr,
+            function (element) {
+                if (shouldPrint(element)) {
+                    console.log(element);
+                }
+            });
+}
+
+// param1: an array of values
+// param2: a function
+//         -Takes the current accumulator
+//         -Takes the current element in the array
+//         Returns the new value of the accumulator
+// param3: starting value
+function reduce(arr, func, startingValue) {
+    let accumulator = startingValue;
+    for (let index = 0; index < arr.length; index++) {
+        let elem = arr[index];
+        accumulator = func(accumulator, elem);
+    }
+    return accumulator;
+}
+
+function product(arr) {
+    return reduce(arr, (a, e) => a * e, 1);
+    // let retval = 1; // accumulator
+    // for (let index = 0; index < arr.length; index++) {
+    //     let elem = arr[index];
+    //     retval = retval * elem;
+    // }
+    // return retval;
+}
+function sum(arr) {
+    return reduce(arr, (a, e) => a + e, 0);
+    // let retval = 0;
+    // for (let index = 0; index < arr.length; index++) {
+    //     let elem = arr[index];
+    //     retval = retval + elem;
+    // }
+    // return retval;
+}
+
