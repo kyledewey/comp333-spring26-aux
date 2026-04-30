fa44e490335fab50ef854eab11657f7484f32075
Kyle Dewey <kyle.dewey@csun.edu>
2026-04-29 17:21:06 -0700

    .

---
+++ section_1/javascript/objects.js
@@ -0,0 +1,12 @@
+// FOR MONDAY: Inheritance
+function makeRectangle(w, h) {
+    return { 'width': w, 'height': h };
+}
+
+// constructor
+function Rectangle(w, h) {
+    // this = {}
+    this.width = w;
+    this.height = h;
+    // return this;
+}
---
+++ section_2/javascript/objects.js
@@ -0,0 +1,18 @@
+// NEXT TIME: inheritance
+function add(x, y) {
+    temp = x + y;
+    return temp;
+}
+
+function makeRectangle(w, h) {
+    // this = {};
+    //return { 'width': w, 'height': h };
+}
+
+// constructor
+function Rectangle(w, h) {
+    // this = {};
+    this.width = w;
+    this.height = h;
+    // return this;
+}
