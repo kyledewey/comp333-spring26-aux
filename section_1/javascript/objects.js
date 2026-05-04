0d10469c923ff53d2e65639c6cafc1b6fa951caf
Kyle Dewey <kyle.dewey@csun.edu>
2026-05-04 15:50:28 -0700

    .

--- section_1/javascript/objects.js
+++ section_1/javascript/objects.js
@@ -3,10 +3,22 @@ function makeRectangle(w, h) {
     return { 'width': w, 'height': h };
 }
 
+let rectanglePrototype = {
+    'getArea': function() {
+        // `this` is a reference to whatever the method
+        // was called on
+        return this.width * this.height;
+    },
+    'getWidth': function() {
+        return this.width;
+    }
+};
+
 // constructor
 function Rectangle(w, h) {
     // this = {}
     this.width = w;
     this.height = h;
+    this.__proto__ = rectanglePrototype;
     // return this;
 }
